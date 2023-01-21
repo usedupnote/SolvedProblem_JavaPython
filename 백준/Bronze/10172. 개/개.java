7ba7983868b59253959d3e87f529a9e1d9df060f
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-21 21:32:44 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n" + "|q p|   /}\n" + "( 0 )\"\"\"\\\n" + "|\"^\"`    |\n" + "||_/=\\\\__|");
+	}
+}
