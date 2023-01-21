1852a6efe7d1e01e8df5be86d4104f4b7b547a47
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-21 22:07:27 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -4,18 +4,29 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		long score = sc.nextLong();
+		int score = sc.nextInt() / 10;
 		
-		char result = 'F';
+		char result = 'A';
 		
-		if (score >= 90) {
+		switch (score) {
+		case 10:
 			result = 'A';
-		} else if (score >= 80) {
+			break;
+		case 9:
+			result = 'A';
+			break;
+		case 8:
 			result = 'B';
-		} else if (score >= 70) {
+			break;
+		case 7:
 			result = 'C';
-		} else if (score >= 60) {
+			break;
+		case 6:
 			result = 'D';
+			break;
+		default:
+			result = 'F';
+			break;
 		}
 		System.out.println(result);
 	}
