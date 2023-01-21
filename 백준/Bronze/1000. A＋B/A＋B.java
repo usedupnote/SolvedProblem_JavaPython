92a20e9abcfb25eb2ed2f48429e2eb63f366f031
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-21 21:41:35 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
 		
-		int N = sc.nextInt();
-		int K = sc.nextInt();
-        
-        System.out.println(N + K);
-    }
+		System.out.println(A + B);
+	}
 }
