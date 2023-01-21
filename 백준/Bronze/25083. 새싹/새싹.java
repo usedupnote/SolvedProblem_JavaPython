ecd1d134d8435533fe06658e27a88c9653561d5a
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-21 21:35:44 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13884 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -3,6 +3,11 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("         ,r'\"7\n" + "r`-_   ,'  ,/\n"+ " \\. \". L_r'\n"+ "   `~\\/\n"+ "      |\n"+ "      |\n");
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
 	}
 }
