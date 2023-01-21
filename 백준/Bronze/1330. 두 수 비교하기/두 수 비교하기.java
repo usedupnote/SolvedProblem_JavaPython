c53ee5cb5e49565ea30dfdedeb1cdb078669ae4d
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-21 22:02:02 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		
+		String result = "==" ;
+		
+		if (A > B) {
+			result = ">";
+		} else if (A < B) {
+			result = "<";
+		}
+		System.out.println(result);
+	}
+}
