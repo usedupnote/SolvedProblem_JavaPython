8f53fbef6a523c7d39fbd2870c737111e4272496
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-22 19:19:08 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String st = sc.next();
+		
+		System.out.println(st.length());
+
+		sc.close();
+	}
+}
