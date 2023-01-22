6440c90c495da154b1ac6f72c2c33f4cb4096640
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-22 19:21:54 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String st = sc.next();
+		
+		System.out.println((int)st.charAt(0));
+
+		sc.close();
+	}
+}
