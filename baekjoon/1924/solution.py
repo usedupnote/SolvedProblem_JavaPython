ecd24980d5bd04e37021a62e1a41dffe351fc88a
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-24 20:29:19 +0900

    uploaded at 2023. 1. 24. 오후 8:29:18

---
+++ baekjoon/1924/solution.py
@@ -0,0 +1,25 @@
+// [문제 링크]: https://www.acmicpc.net/problem/1924
+
+import sys
+input = sys.stdin.readline
+​
+month, day = map(int, input().split())
+​
+result = 0
+​
+day30Month = [4, 6, 9, 11]
+​
+week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+​
+for i in range(1, month) :
+​
+    if i == 2:
+        result += 28
+    elif i in day30Month:
+        result += 30
+    else:
+        result += 31
+​
+result += day
+​
+print(week[result%7])
