943479dbc9f730a024484a3f2c9fa5d41ae1c3bf
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-26 21:29:09 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+i = 1
+result = 0
+
+for i in range(1, N//2 + 1) :
+    if N % i == 0 :
+        K -= 1
+
+    if K <= 0 :
+        result = i
+        break
+
+    i += 1
+
+if result == 0 :
+    if K <= 1 :
+        result = N
+
+print(result)
