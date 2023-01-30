0c85f14c76e3a509de8d2ad5ebe2d5edfe7575ff
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-30 22:42:38 +0900

    uploaded at 2023. 1. 30. 오후 10:42:37

---
+++ baekjoon/10189/solution.py
@@ -0,0 +1,9 @@
+// [문제 링크]: https://www.acmicpc.net/problem/10189
+
+import sys
+input = sys.stdin.readline
+​
+print("#  # #### #### #  #")
+print("#### #  # #  # # #")
+print("#### #  # #  # # #")
+print("#  # #### #### #  #")
