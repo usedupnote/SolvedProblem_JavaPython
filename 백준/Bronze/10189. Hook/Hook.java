5c72c9e9ec8fd8fed5377bef13cfd73299a7ac93
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-01-30 22:55:45 +0900

    [Bronze V] Title: Hook, Time: 112 ms, Memory: 13824 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {		
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+
+        sb.append("#  # #### #### #  #\n")
+		.append("#### #  # #  # # #\n")
+		.append("#### #  # #  # # #\n")
+		.append("#  # #### #### #  #");
+		
+		System.out.println(sb.toString());
+	}
+}
