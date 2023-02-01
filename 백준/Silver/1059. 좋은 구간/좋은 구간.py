181d19f212d43c2bd6e37119b31251537efa855c
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-01 21:23:47 +0900

    [Silver IV] Title: 좋은 구간, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+L = int(input())
+
+S = list(map(int, input().split()))
+
+n = int(input())
+
+minimum = 0
+maximum = 1000
+
+for i in S :
+
+    if i == n :
+        maximum = n
+        minimum = n
+        break
+    elif i < n :
+        minimum = max(minimum, i)
+    else :
+        maximum = min(maximum, i)
+
+
+
+if maximum - minimum < 2 :
+    print(0)
+else :
+    minimum = n - (minimum + 1)
+    maximum = (maximum - 1) - n
+
+    print((minimum * maximum) + (minimum + maximum))
