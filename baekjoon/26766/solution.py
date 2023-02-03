7b6cd942d647c0a88a6f82c2cf65dc3a0571bbe8
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-03 22:55:38 +0900

    uploaded at 2023. 2. 3. 오후 10:55:37

---
+++ baekjoon/26766/solution.py
@@ -0,0 +1,17 @@
+// [문제 링크]: https://www.acmicpc.net/problem/26766
+
+import sys
+input = sys.stdin.readline
+​
+N = int(input())
+​
+for i in range(N) :
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
