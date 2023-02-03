8bf776da24c24383da66e4b25ec4a839936ed7ec
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-03 22:55:41 +0900

    [Bronze V] Title: Serca, Time: 452 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(N) :
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
