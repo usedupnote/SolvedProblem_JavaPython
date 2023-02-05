40e4bec705090e662f437c2cab215429c182aec0
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-05 11:21:13 +0900

    uploaded at 2023. 2. 5. 오전 11:21:13

---
+++ baekjoon/16099/solution.py
@@ -0,0 +1,16 @@
+// [문제 링크]: https://www.acmicpc.net/problem/16099
+
+import sys
+input = sys.stdin.readline
+​
+N = int(input())
+​
+for i in range(N) :
+    num = list(map(int, input().split()))
+​
+    if num[0] * num[1] > num[2] * num[3] :
+        print("TelecomParisTech")
+    elif num[0] * num[1] == num[2] * num[3] :
+        print("Tie")
+    else :
+        print("Eurecom")
