89f6bcaed893125587783c95ad154de7ec8fd271
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-06 20:47:53 +0900

    uploaded at 2023. 2. 6. 오후 8:47:52

---
+++ baekjoon/1003/solution.py
@@ -0,0 +1,20 @@
+// [문제 링크]: https://www.acmicpc.net/problem/1003
+
+import sys
+input = sys.stdin.readline
+​
+count = [[1, 0], [0, 1]]
+result = []
+​
+T = int(input())
+​
+for i in range(T) :
+    result.append(int(input()))
+​
+maximum = max(result)
+​
+for i in range(1, maximum) :
+    count.append([count[i][0] + count[i-1][0], count[i][1] + count[i-1][1]])
+​
+for i in result :
+    print(*count[i])
