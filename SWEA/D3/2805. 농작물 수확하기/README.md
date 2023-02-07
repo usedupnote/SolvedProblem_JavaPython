eb0562391072df256a560750caff75c6167cb6e3
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-07 16:42:30 +0900

    [D3] Title: 농작물 수확하기, Time: 135 ms, Memory: 21,040 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/README.md
@@ -0,0 +1,11 @@
+# [D3] 농작물 수확하기 - 2805 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GLXqKAWYDFAXB) 
+
+### 성능 요약
+
+메모리: 21,040 KB, 시간: 135 ms, 코드길이: 1,210 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int T;
+	static int N;
+	static int corp;
+	static int f, l;
+	static int result;
+	static char[] ch;
+	
+	public static void main(String[] args) throws IOException {
+		// 입출력을 위한 기본 설정
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		// test_case 갯수 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		T = Integer.parseInt(st.nextToken());
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			result = 0;
+			
+			st = new StringTokenizer(br.readLine());
+			
+			N = Integer.parseInt(st.nextToken());
+			
+			f = (N + 1)/ 2 - 1;
+			l = (N + 1)/ 2 - 1;
+			ch = new char[N];
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				ch = st.nextToken().toCharArray();
+				
+				for (int j = 0; j < N; j++) {
+					corp = Character.valueOf(ch[j]) - '0';
+					
+					if(f >= j && l <= j) {
+						result += corp;
+					}
+				}
+				if (i < (N + 1)/ 2 - 1) {
+					f++;
+					l--;
+				} else {
+					f--;
+					l++;
+				}
+			}
+			sb.append("#" + test_case + " "+result + "\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
