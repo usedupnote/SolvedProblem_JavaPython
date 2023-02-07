e3ad3021af1cefcbcae2714b7ec806841b892d32
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-07 15:40:55 +0900

    [Silver III] Title: N과 M (1), Time: 2184 ms, Memory: 184428 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int M;
+	static boolean[] check;
+	static ArrayList<Integer> arr = new ArrayList<>();
+	
+	static void NandM(int a, int b) {
+		for (int i = 0; i < a; i++) {
+			if (b > 0) {
+				if(check[i] == false) {
+					arr.add(i + 1);
+					check[i] = true;
+					NandM(a, b-1);
+					arr.remove(arr.size() - 1);
+					check[i] = false;
+				}
+			} else {
+				System.out.println(arr.toString().replace("[", "").replace(",", "").replace("]", ""));
+				break;
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		// 입출력을 위한 기본 설정
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		// 자연수 N과 M 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		check = new boolean[N];
+
+		NandM(N, M);
+	}
+}
