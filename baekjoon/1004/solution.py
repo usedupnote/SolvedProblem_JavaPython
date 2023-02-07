1da7db32546e0984d65ba11e00f41cb5be29655c
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-07 22:55:36 +0900

    uploaded at 2023. 2. 7. 오후 10:55:35

---
+++ baekjoon/1004/solution.py
@@ -0,0 +1,26 @@
+// [문제 링크]: https://www.acmicpc.net/problem/1004
+
+import sys
+input = sys.stdin.readline
+​
+T = int(input())
+​
+for test_case in range(T) :
+    x1, y1, x2, y2 = map(int, input().split())
+​
+    N = int(input())
+​
+    result = 0
+​
+    for i in range(N) :
+        cx, cy, r = map(int, input().split())
+​
+        check = 0
+​
+        if (r * r) < ((cx - x1) * (cx - x1)) + ((cy - y1) * (cy - y1)) :
+            check += 1
+        if (r * r) < ((cx - x2) * (cx - x2)) + ((cy - y2) * (cy - y2)) :
+            check += 1
+​
+        result += (check % 2)
+    print(result)
