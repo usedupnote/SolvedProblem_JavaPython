ed9736738ba21ab31d24ca68cc2f043c6edc53b0
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-08 23:02:14 +0900

    uploaded at 2023. 2. 8. 오후 11:02:13

---
+++ baekjoon/15651/solution.py
@@ -0,0 +1,19 @@
+// [문제 링크]: https://www.acmicpc.net/problem/15651
+
+import sys
+input = sys.stdin.readline
+​
+num = []
+​
+def NandM(n, m) :
+    if m == 0 :
+        print(*num)
+        return
+    for i in range(N) :
+        num.append(i + 1)
+        NandM(i, m - 1)
+        num.pop()
+​
+N, M = map(int, input().split())
+​
+NandM(N, M)
