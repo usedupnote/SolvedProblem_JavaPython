767dc3a477a2b4968c5bd4f39fed77bd305c4238
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-09 11:48:03 +0900

    [Silver IV] Title: 토너먼트, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -2,27 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int team;
-	static int kim;
-	static int im;
+	static int team, kim, im;
 	static int count;
-	
-	static void tonument(int A, int B) {
-		if (A == B) return;
 
-		count++;
-		tonument(A/2, B/2);
-	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		team = Integer.parseInt(st.nextToken());
 		kim = Integer.parseInt(st.nextToken()) - 1;
 		im = Integer.parseInt(st.nextToken()) - 1;
 		
-		tonument(kim, im);
+		while (kim != im) {
+			count += 1;
+			kim /= 2;
+			im /= 2;
+		}
 		System.out.println(count);
 	}
 }
