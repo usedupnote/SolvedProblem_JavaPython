5b60dee182105b190006664b5173f186b82d12e1
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-13 23:17:38 +0900

    [Bronze II] Title: 트로피 진열, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1668. 트로피 진열/트로피 진열.py
@@ -0,0 +1,18 @@
+N = int(input())
+num = []
+
+R, L = 0, 0
+resultR, resultL = 0, 0
+
+for i in range(N) :
+    num.append(int(input()))
+    if R < num[i] :
+        R = num[i]
+        resultR += 1
+
+for i in range(N) :
+    if L < num[N - 1 - i] :
+        L = num[N - 1 - i]
+        resultL += 1
+
+print(resultR, resultL, sep="\n")
