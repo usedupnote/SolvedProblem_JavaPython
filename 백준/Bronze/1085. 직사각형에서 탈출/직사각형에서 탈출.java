9650db29b93e3311567deefc52dafe669b9617d2
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-14 17:44:57 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int x, y, w, h;
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		w = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+		
+		System.out.println(Math.min(Math.min(x, y), Math.min(w - x, h - y)));
+		br.close();
+	}
+}
