dbf6150c150a6e03ac69a4002545f44c22ef9673
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-14 21:36:28 +0900

    uploaded at 2023. 2. 14. 오후 9:36:27

---
+++ baekjoon/2755/solution.py
@@ -0,0 +1,23 @@
+// [문제 링크]: https://www.acmicpc.net/problem/2755
+
+N = int(input())
+​
+score = {"A+" : 4.3, "A0" : 4.0, "A-": 3.7, "B+": 3.3, "B0": 3.0, "B-": 2.7, "C+": 2.3, "C0": 2.0, "C-": 1.7, "D+": 1.3, "D0": 1.0, "D-": 0.7, "F" : 0}
+A = 0
+count = 0
+​
+for i in range(N) :
+    s = input().split(" ")
+​
+    check = s[len(s) - 1]
+​
+    A += int(s[len(s) - 2]) * score[check] * 1000
+    count += int(s[len(s) - 2])
+​
+result = A/count
+​
+if result % 10 >= 5 :
+    result += 10
+    result -= result % 10
+​
+print(f"{result/1000:.2f}")
