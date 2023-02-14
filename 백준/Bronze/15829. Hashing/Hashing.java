5a5a9c2753fbf48f8c2b2b0b7fab5b9a1ca0bdbd
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-14 10:54:42 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+	static int N;
+	static long result;
+	static int[] num;
+	static char[] ch;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		ch = br.readLine().toCharArray();
+		
+		for (int i = 0; i < N; i++) {
+			result += (Character.valueOf(ch[i])-'a' + 1) * Math.pow(31, i);
+			result %= 1234567891;
+		}
+		
+		System.out.println(result);
+		br.close();
+	}
+}
