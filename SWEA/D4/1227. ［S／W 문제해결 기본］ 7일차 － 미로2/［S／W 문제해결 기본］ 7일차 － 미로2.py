7a37c3a4bd6b76e6119dfe79ae8558c28ce45c29
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-15 17:06:14 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 244 ms, Memory: 64,360 KB -BaekjoonHub

--- SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.py
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.py
@@ -1,42 +1,58 @@
-for test_case in range(1, 11) :
-    section = int(input())
-    map = [list(input()) for _ in range(100)]
-    savepoint = [[1, 1]]
-    nowpoint = [1, 1]
+import java.io.*;
+import java.util.*;
 
-    map[1][1] = "1"
-
-    result = 0
-
-    while True:
-        x = nowpoint[0]
-        y = nowpoint[1]
-
-        if int(map[x+1][y]) + int(map[x-1][y]) + int(map[x][y+1]) + int(map[x][y-1]) < 3 :
-            savepoint.append([x, y])
-
-        if map[x][y] == "3" :
-            result = "1"
-            break
-
-        map[x][y] = "1"
-
-        if map[x+1][y] != "1" :
-            nowpoint[0] += 1
-
-        elif map[x][y-1] != "1" :
-            nowpoint[1] -= 1
-
-        elif map[x][y+1] != "1" :
-            nowpoint[1] += 1
-
-        elif map[x-1][y] != "1" :
-            nowpoint[0] -= 1
-
-        else :
-            if len(savepoint) == 0 :
-                break
-            else :
-                nowpoint = savepoint.pop()
-
-    print("#{} {}".format(section, result))
+public class Solution {
+	
+	static int result;
+	static final int[] DIRX = {1, 0, -1, 0}; //상 우 하 좌
+	static final int[] DIRY = {0, 1, 0, -1};
+	static int[] startpoint;
+	static int[][] map;
+		
+	static void dfs(int x, int y) {
+		if(map[x][y] == 3) {
+			result = 1;
+			return;
+		}
+		map[x][y] = 1;
+		
+		for (int d = 0; d < 4; d++) {
+			int dx = x + DIRX[d];
+			int dy = y + DIRY[d];
+			if (map[dx][dy] != 1) {
+				dfs(dx, dy);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int T = Integer.parseInt(st.nextToken());
+			
+			result = 0;
+			map = new int[100][100];
+			int[] startpoint = new int[2];
+			
+			for (int i = 0; i < 100; i++) {
+				char[] ch = br.readLine().toCharArray();
+				for (int j = 0; j < 100; j++) {
+					map[i][j] = Character.valueOf(ch[j]) - '0';
+					if (map[i][j] == 2) {
+						startpoint[0] = i;
+						startpoint[1] = j;
+					}
+				}
+			}
+			
+			dfs(startpoint[0], startpoint[1]);
+			
+			sb.append("#"+test_case+" " + result +"\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+}
