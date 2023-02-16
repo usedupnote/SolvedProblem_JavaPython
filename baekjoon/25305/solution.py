79a6750b87b7ae3c41c7264c88033453f9990cb2
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-16 23:16:21 +0900

    uploaded at 2023. 2. 16. 오후 11:16:21

---
+++ baekjoon/25305/solution.py
@@ -0,0 +1,23 @@
+// [문제 링크]: https://www.acmicpc.net/problem/25305
+
+import sys
+input = sys.stdin.readline
+​
+scoreSet = [0 for _ in range(10001)]
+​
+N, M = map(int, input().split())
+​
+score = list(map(int, input().split()))
+​
+for i in range(N) :
+    scoreSet[score[i]] += 1
+​
+result = 0
+​
+for i in range(10000, -1, -1) :
+    M -= scoreSet[i]
+    if(M <= 0) :
+        result = i
+        break
+​
+print(result)
