44be4f45319e75c671e3925be9afcf0f9933ab54
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-17 23:35:25 +0900

    uploaded at 2023. 2. 17. 오후 11:35:25

---
+++ baekjoon/27434/solution.py
@@ -0,0 +1,12 @@
+// [문제 링크]: https://www.acmicpc.net/problem/27434
+
+import sys
+input = sys.stdin.readline
+​
+N = int(input())
+result = 1
+​
+for i in range(1, N + 1) :
+    result *= i
+​
+print(result)
