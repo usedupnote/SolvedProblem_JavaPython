4823033c2035b5d67fde0495edbd3b52904587e8
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-26 18:50:22 +0900

    uploaded at 2023. 2. 26. 오후 6:50:14

---
+++ baekjoon/10178/solution.py
@@ -0,0 +1,10 @@
+// [문제 링크]: https://www.acmicpc.net/problem/10178
+
+import sys
+input = sys.stdin.readline
+​
+T = int(input())
+​
+for test_case in range(T) :
+    N, M = map(int, input().split())
+    print("You get",N//M , "piece(s) and your dad gets",N % M, "piece(s).")
