3c0763b55efd2a3fb483f6926d21ad89e91c7f12
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-27 23:49:13 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 668 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+num = list(map(int, input().split()))
+
+result = 4
+while True :
+    cnt = 0
+
+    for i in range(5) :
+        if result % num[i] == 0 :
+            cnt += 1
+
+    if cnt >= 3 :
+        break
+
+    result += 1
+print(result)
