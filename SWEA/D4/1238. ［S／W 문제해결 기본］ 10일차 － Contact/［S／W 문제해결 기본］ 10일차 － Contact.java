c9b615903d673ab01b0bfdbc4f3b094014714f5a
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-27 16:08:56 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 111 ms, Memory: 18,884 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -18,7 +18,6 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			
-			// size로 묶어서 루프
 			arrlist = new ArrayList[101];
 			
 			// 이거 안하면 nullPoint 떨어짐
@@ -39,7 +38,8 @@ public class Solution {
 			count = 0;
 			result = 0;
 
-			bfs(M);
+			//bfs1(M);
+			bfs2(M, 1);
 			
 			sb.append("#" + test_case+ " " + result + "\n");
 		}
@@ -47,7 +47,7 @@ public class Solution {
 		br.close();
 	}
 	
-	static void bfs(int start) {
+	static void bfs1(int start) {
 		Queue<Integer> queue = new ArrayDeque<Integer>();
 		int[] visited = new int[101];
 		
@@ -58,6 +58,7 @@ public class Solution {
 		
 		while (!queue.isEmpty()) {
 			current = queue.poll();
+			System.out.println(current);
 			
 			for (int vertex : arrlist[current]) {
 				if(visited[vertex] == 0) {
@@ -72,7 +73,36 @@ public class Solution {
 			}
 			else if(count == visited[current]) {
 				result = Math.max(result, current);
-				count = visited[current];
+			}
+		}
+	}
+	
+	static void bfs2(int start, int cnt) {
+
+		Queue<int[]> queue = new ArrayDeque<int[]>();
+		boolean[] visited = new boolean[101];
+		
+		queue.offer(new int[] {start, cnt});
+		visited[start] = true;
+		
+		int[] current;
+		
+		while (!queue.isEmpty()) {
+			current = queue.poll();
+			
+			if (count < current[1]) {
+				result = current[0];
+				count = current[1];
+			}
+			else if(count == current[1]) {
+				result = Math.max(result, current[0]);
+			}
+			
+			for (int vertex : arrlist[current[0]]) {
+				if(visited[vertex]) continue;
+
+				visited[vertex] = true;
+				queue.offer(new int[] {vertex, current[1] + 1});
 			}
 		}
 	}
