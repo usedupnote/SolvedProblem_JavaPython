72c8a4018d7fd308c2f055f871bba280d8ec5c19
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-28 23:46:09 +0900

    uploaded at 2023. 2. 28. 오후 11:46:08

---
+++ baekjoon/1946/solution.py
@@ -0,0 +1,23 @@
+// [문제 링크]: https://www.acmicpc.net/problem/1946
+
+import sys
+input = sys.stdin.readline
+​
+T = int(input())
+​
+for test_case in range(T) :
+    N = int(input())
+​
+    num = [0 for _ in range(N)]
+    for i in range(N) :
+        person = list(map(int, input().split()))
+        num[person[0] - 1] = person[1] - 1
+​
+    check = num[0]
+    result = 1
+    for i in range(1, N) :
+        if(num[i] < check) :
+            check = num[i]
+            result += 1
+​
+    print(result)
