6cb9d4db2b41b3dec2c41c5b098d045207af5cba
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-02-28 13:22:38 +0900

    [Bronze V] Title: A+B - 8, Time: 176 ms, Memory: 16504 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(st.nextToken());
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			System.out.println("Case #"+test_case+": "+N+" + "+M+" = " + (N + M));
+		}
+	}
+}
