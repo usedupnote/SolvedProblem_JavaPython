07ec9ba6ec54661e4e0ccbc1765e7fac8bfd4a45
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-07 11:03:39 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 5,557 ms, Memory: 30,380 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -2,70 +2,79 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static final int[] dirX = {-1, 0, 1, 0}; // 상 하 우 좌
-	static final int[] dirY = {0, 1, 0, -1};
+	static int N;
 	static int[][] map;
-	static int[][] v;
+	static int[] dirX = {-1,1,0,0};
+	static int[] dirY = {0,0,-1,1};
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int C = Integer.parseInt(st.nextToken());
-		
-		for (int test_case = 1; test_case <= C; test_case++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			
-			map = new int[N][N];
-			v = new int[N][N];
-			
-			for (int i = 0; i < N; i++) {
-				char[] ch = br.readLine().toCharArray();
-				for (int j = 0; j < N; j++) {
-					map[i][j] = ch[j] - '0';
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int TC = Integer.parseInt(st.nextToken());
+        
+        for (int test_case = 1; test_case <= TC; test_case++) {
+        	st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+	        
+            map = new int[N][N];
+            for (int i = 0; i < N; i++) {
+            	char[] ch = br.readLine().toCharArray();
+                for (int j = 0; j < N; j++) {
+                	map[i][j] = ch[j] - '0';
 				}
 			}
-			
-			int maxV = Integer.MAX_VALUE;
-			
-			for (int i = 0; i < N; i++) {
-				Arrays.fill(v[i], maxV);
-			}
-
-			v[0][0] = 0;
-			
-			while (true) {
-				// step 1 : 경유지로 처리되지 않은 정점 중 출발지에서 가장 가까운 정점 선택
-				int[] current = {0, 0};
-				int min = Integer.MAX_VALUE;
-				
-				for (int i = 0; i < N; i++) {
-					for (int j = 0; j < N; j++) {
-						if(v[i][j] != maxV) {
-							for (int d = 0; d < 4; d++) {
-								int dx = i + dirX[d];
-								int dy = j + dirY[d];
-								
-								if(0<=dx && dx<N && 0<=dy && dy<N && v[dx][dy] == maxV && min > map[dx][dy] + v[i][j]) {
-									current = new int[] {dx, dy};
-									min = map[dx][dy] + v[i][j];
-								}
-							}
-						}
-					}
-				}
-				
-				// step 2 : 위에서 선택된 정점을 경유지로 해서 갈 수 있는 다른 인점 정점과의 비용 최소값 갱신		
-				if(v[current[0]][current[1]] > min) {
-					v[current[0]][current[1]] = min;
-				}
-				if(current[0] == (N-1) && current[1] == (N-1)) break;
-			}
-			
-			sb.append("#" + test_case + " " + v[N-1][N-1] + "\n");
+            System.out.println("#" + test_case + " " + dijkstra());
 		}
-		System.out.println(sb);
-		br.close();
+	}
+
+	static int dijkstra() {
+		final int INF = Integer.MAX_VALUE;
+		int[][] minTime = new int[N][N];
+		boolean[][] visited = new boolean[N][N];
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return Integer.compare(o1[2], o2[2]);
+			}
+		}); // r,c,출발지에서 자신까지 최소비용
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				minTime[i][j] = INF;
+			}
+		}
+		
+		minTime[0][0] = 0;
+		pq.offer(new int[] {0,0,minTime[0][0]});
+		
+		int[] cur = null;
+		int r, c, minCost;
+		
+		while (true) {
+			//step 1
+			cur = pq.poll();
+			r = cur[0];
+			c = cur[1];
+			minCost = cur[2];
+			
+			if(visited[r][c]) continue;
+			visited[r][c] = true;
+			if(r == N-1 && c == N-1) return minCost;
+			
+			//step 2
+			int nr = 0, nc = 0;
+			for (int d = 0; d < 4; d++) {
+				nr = r + dirX[d];
+				nc = c + dirY[d];
+				
+				if(0<=nr && nr<N && 0<=nc && nc<N && !visited[nr][nc] && minTime[nr][nc] > minCost+ map[nr][nc]){
+					minTime[nr][nc] = minCost + map[nr][nc];
+					pq.offer(new int[] {nr, nc, minTime[nr][nc]});
+				}
+			}
+		}
 	}
 }
