5f7a950302ce9ee9ac9e96784b47fd23e8316d44
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-09 14:59:40 +0900

    [Silver III] Title: 퇴사, Time: 120 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,23 +1,25 @@
+import java.io.*;
 import java.util.*;
-
+ 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(st.nextToken());
 		
 		int[] day = new int[N + 1];
 		
-		int T = sc.nextInt();
-		int P = sc.nextInt();
+		st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
 		
-		if (T <= N) {
-			day[T] = P;
-		}
+		if (T <= N) day[T] = P;
 		
 		for (int i = 1; i < N; i++) {
-			T = sc.nextInt();
-			P = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			T = Integer.parseInt(st.nextToken());
+			P = Integer.parseInt(st.nextToken());
 
 			day[i] = Math.max(day[i-1], day[i]);
 			
@@ -28,5 +30,6 @@ public class Main {
 		day[N] = Math.max(day[N-1], day[N]);
 		
 		System.out.println(day[N]);
+		br.close();
 	}
 }
