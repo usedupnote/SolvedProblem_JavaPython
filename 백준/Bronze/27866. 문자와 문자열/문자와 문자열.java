dfbcc3bd023e0c2901c0afb24959940c9441c09e
leejm9704@naver.com <leejm9704@naver.com>
2023-03-10 23:27:02 +0900

    update: 0310 Solved Problem BJ 27866

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,19 @@
+package com.SSAFY.cd;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		char[] ch = br.readLine().toCharArray();
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		
+		System.out.println(ch[N-1]);
+	}
+}
