b7ea30120b715600f2c47f26095fbf872514d400
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-14 10:28:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		char[] ch = br.readLine().toCharArray();
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		
+		System.out.println(ch[N-1]);
+	}
+}
