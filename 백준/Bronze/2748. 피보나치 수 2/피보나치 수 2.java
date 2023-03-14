4170da812588afa8d27a3ebe79105d31b4eb7054
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-14 10:28:06 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-package com.SSAFY.cd;
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringBuilder sb = new StringBuilder();
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	int N = Integer.parseInt(st.nextToken());
-    	
-    	long[] num = new long[N + 1];
-    	
-    	for (int i = 0; i <= N; i++) {
-			if(i == 0) {
-				num[i] = 0;
-			} else if (i == 1) {
-				num[i] = 1;
-			} else {
-				num[i] = num[i-1] + num[i-2];
-			}
-		}
-    	
-    	System.out.println(num[N]);
-	}
-=======
 import java.io.*;
 import java.util.*;
 
@@ -51,5 +22,4 @@ public class Main {
 		}
     	System.out.println(num[N]);
 	}
->>>>>>> 6a478f6bfe213942b57a00a2cb12d6159551f101
 }
