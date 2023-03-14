1c8809bfee502871dd8d26fd71c2bc2276418a59
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-14 10:28:17 +0900

    [Bronze IV] Title: 빵, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.java
+++ 백준/Bronze/25377. 빵/빵.java
@@ -1,12 +1,9 @@
-package com.SSAFY.cd;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws  IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         
         st = new StringTokenizer(br.readLine());
