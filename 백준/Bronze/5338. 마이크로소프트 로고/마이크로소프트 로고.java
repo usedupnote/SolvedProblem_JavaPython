bec51d6d915c48c28be18c0e26860a0292ea8990
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-20 00:41:23 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+	public static void main(String[] args) {
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("       _.-;;-._\n" + "'-..-'|   ||   |\n" + "'-..-'|_.-;;-._|\n" + "'-..-'|   ||   |\n" + "'-..-'|_.-''-._|");
+    	System.out.print(sb.toString());
+	}
+}
