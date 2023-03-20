d957e555851490aea8c193c5ce99ba3ecb50684f
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-20 23:44:39 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -9,9 +9,10 @@ public class Main {
     	int N = Integer.parseInt(st.nextToken());
     	int[] num = new int[N];
     	st = new StringTokenizer(br.readLine());
-    	for (int i = 0; i < N; i++) {
+    	num[0] = Integer.parseInt(st.nextToken());
+    	for (int i = 1; i < N; i++) {
     		num[i] = Integer.parseInt(st.nextToken());
-    		for (int j = 0; j < i; j++) {
+    		for (int j = 0; j <= i / 2; j++) {
 				num[i] = Math.max(num[i], num[j] + num[i - j - 1]);
 			}
 		}
