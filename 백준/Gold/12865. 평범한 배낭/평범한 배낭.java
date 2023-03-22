ff9934f2ce6f587e73a02bb0750bd8b36aad8aa4
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-22 23:26:14 +0900

    [Gold V] Title: 평범한 배낭, Time: 232 ms, Memory: 53488 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, K, result;
+	static ArrayDeque<int[]> ad;
+	static int[][] bag;
+
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());	//물품의 수 N(1 ≤ N ≤ 100)
+    	K = Integer.parseInt(st.nextToken());	//버틸 수 있는 무게 K(1 ≤ K ≤ 100,000)
+    	
+    	ad = new ArrayDeque<>();
+    	
+    	for (int i = 0; i < N; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int W = Integer.parseInt(st.nextToken()); //물건의 무게 W(1 ≤ W ≤ 100,000)
+    		int V = Integer.parseInt(st.nextToken()); //해당 물건의 가치 V(0 ≤ V ≤ 1,000)
+			ad.offer(new int[] {W, V});
+		}
+    	
+    	bag = new int[N][K + 1];
+    	
+    	int[] poll = ad.poll();
+    	for (int i = 0; i <= K; i++) {
+			if(i >= poll[0]) {
+				bag[0][i] = poll[1];
+			}
+		}
+    	
+    	if(N != 1) {
+        	for (int i = 1; i < N; i++) {
+        		poll = ad.poll();
+    			for (int j = 0; j <= K; j++) {
+    				if(j >= poll[0]) {
+    					bag[i][j] = Math.max(bag[i-1][j], bag[i-1][j - poll[0]] + poll[1]);
+    				} else {
+    					bag[i][j] = bag[i-1][j];
+    					
+    				}
+    			}
+    		}
+    	}
+		//한 줄에 배낭에 넣을 수 있는 물건들의 가치합의 최댓값을 출력한다.
+    	System.out.println(bag[N -1][K]);
+    	br.close();
+	}
+}
