de8f6063ec120da200f6dda2ff281a3446ad8e5b
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-28 11:09:48 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -4,19 +4,14 @@ import java.util.*;
 public class Main {	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         int N = Integer.parseInt(st.nextToken());
         
-        int[][] map = new int[N][3];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 3; i++) {
-			map[0][i] = Integer.parseInt(st.nextToken());
-		}
+        int[][] map = new int[N+1][3];
 
         int[] current = new int[3];
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
         	st = new StringTokenizer(br.readLine());
         	for (int j = 0; j < 3; j++) {
         		current[j] = Integer.parseInt(st.nextToken());
@@ -25,6 +20,6 @@ public class Main {
 			map[i][1] = current[1] + Math.min(map[i-1][0], map[i-1][2]);
 			map[i][2] = current[2] + Math.min(map[i-1][0], map[i-1][1]);
 		}
-        System.out.println(Math.min(map[N-1][0], Math.min(map[N-1][1], map[N-1][2])));
+        System.out.println(Math.min(map[N][0], Math.min(map[N][1], map[N][2])));
 	}
 }
