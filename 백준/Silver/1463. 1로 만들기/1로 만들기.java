086c461da8e8b789260a3815597aac23afedce32
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-28 15:47:37 +0900

    [Silver III] Title: 1로 만들기, Time: 200 ms, Memory: 34292 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,26 +2,21 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, result;
-	static int[] v;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		
+
+		int N = Integer.parseInt(st.nextToken());
 		ArrayDeque<Integer> q = new ArrayDeque<>();
-        
+
+		int result = 0;
 		q.offer(N);
 		
-		v = new int[1000001];
+		int[] v = new int[N+2];
 		
 		while (!q.isEmpty()) {
 			int poll = q.poll();
-			
-			if (poll == 1) {
-				result = v[poll];
-			}
+			if (poll == 1) result = v[poll];
 			
 			if(poll % 3 == 0 && v[poll / 3] == 0) {
 				v[poll / 3] += v[poll] + 1;
