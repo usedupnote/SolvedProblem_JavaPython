c0e28039015e08791742c562a2802c247f2a712e
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-28 16:19:56 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -4,18 +4,20 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());	//TestCase 수
 		
-		int[] map = new int[n+1];
-		
+		long[] map = new long[n+1];
 		map[0] = 1; map[1] = 3;
+		long maxv = Long.MAX_VALUE/2;
 		
 		for (int i = 2; i < n; i++) {
-			map[i] = (map[i-1] + (2 * map[i-2]))%10007;
+			map[i] = map[i-1] + (2 * map[i-2]);
+			if(map[i] > maxv) {
+				map[i] %= 10007;
+			}
 		}
-		System.out.println(map[n-1]);
+		System.out.println((map[n-1])%10007);
 	}
 }
