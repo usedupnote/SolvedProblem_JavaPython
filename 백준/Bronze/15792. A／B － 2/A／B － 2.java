be4994f47f310a5c0539104639fd14ae0043e01b
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-29 10:55:57 +0900

    [Bronze I] Title: A/B - 2, Time: 152 ms, Memory: 16332 KB -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		sb.append(A/B + ".");
+		
+		int cnt = 0;
+		
+		while (cnt++ < 1999) {
+			A %= B;
+			A *= 10;
+			if(A== 0) break;
+			sb.append(A/B);
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+}
