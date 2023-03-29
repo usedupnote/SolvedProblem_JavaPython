6daeaa1ee3331475df47300af535a391de9591e4
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-29 10:18:50 +0900

    [Silver IV] Title: 설탕 배달 2, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/26099. 설탕 배달 2/설탕 배달 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		long N = Long.parseLong(st.nextToken());	// 배송할 설탕량 입력
+		
+		long result = -1;		// 결과값 출력을 위한 값 초기화
+								// (값 재할당이 안될 경우 -1로 출력하기 위해 -1로 초기화)
+	
+		for (int i = 0; i < 5; i++) {
+			if((N-(i * 3)) % 5 == 0 && (N-(i * 3)) >= 0) {    // 3kg 봉투의 갯수에 따라서 나누어떨어지는지 확인
+				result = (N+(i * 2)) / 5;    // 결과값에 전체 봉투의 개수 출력				}
+				break; // 반복문 탈출
+			}
+		}
+		System.out.println(result);
+		br.close();
+	}
+}
