80e914a586c7075b46788e9d500134bbd3cebf3f
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-29 15:25:49 +0900

    [Silver II] Title: 스타트와 링크, Time: 144 ms, Memory: 14820 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -2,61 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;
-	static int result, teamA, teamB;
-	static int[][] map;
-	static boolean[] v;
-	
-	static void comb(int cnt, int start) {
-		if(cnt == N/2) {
-			teamA = 0;
-			teamB = 0;
-			
-			for (int i = 0; i < N - 1; i++) {
-				for (int j = i; j < N; j++) {
-					if( v[i] && v[j]) teamA += map[i][j];
-					if(!v[i] &&!v[j]) teamB += map[i][j];
-				}
-			}
-			result = Math.min(result, Math.abs(teamA - teamB));
-			return;
-		}
-		
-		for (int i = start; i < N ; i++) {
-			if(v[i]) continue;
-			v[i] = true;
-			comb(cnt + 1, i + 1);
-			v[i] = false;
-		}
-	}
-
+    private static int N;
+    private static int[] sumRow, sumColumn;
+    private static int result = Integer.MAX_VALUE;
+    
 	public static void main(String[] args) throws IOException {
-		// 초기 설정 부분
-		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		N = Integer.parseInt(st.nextToken());
 		
-		result = Integer.MAX_VALUE;
-		
-		v = new boolean[N];
-		map = new int[N][N];
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				if (i > j) {
-					map[j][i] += Integer.parseInt(st.nextToken());
-				} else {
-					map[i][j] += Integer.parseInt(st.nextToken());
-				}
-			}
-		}
-		
-		comb(0, 1);
-		
-		System.out.println(result);
-		br.close();
+        sumRow 	  = new int[N];
+        sumColumn = new int[N];
+
+        int sumTotal = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                sumTotal += num;
+                sumRow[i] += num;
+                sumColumn[j] += num;
+            }
+        }
+
+        comb(0, 0, sumTotal);
+        
+        System.out.println(result);
+        br.close();
 	}
+
+    private static void comb(int index, int cnt, int sumTotal) {
+        if (cnt == (N/2)) {
+        	result = Math.min(result, Math.abs(sumTotal));
+        	return;
+        } else if(index == N) {
+        	return;
+        }
+        comb(index + 1, cnt + 1, sumTotal - sumRow[index] - sumColumn[index]);
+        comb(index + 1, cnt, sumTotal);
+    }
 }
