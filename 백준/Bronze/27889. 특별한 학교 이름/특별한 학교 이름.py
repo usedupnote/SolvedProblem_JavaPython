6710afda9df8808c0ab42ac38ec6d1df6b852bb0
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-03-30 23:30:40 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -1,5 +1,2 @@
-import sys
-input = sys.stdin.readline
-name = input().split()
 school = {"NLCS": "North London Collegiate School", "BHA": "Branksome Hall Asia", "KIS": "Korea International School", "SJA": "St. Johnsbury Academy"}
-print(school[name[0]])
+print(school[input()])
