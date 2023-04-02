b26d3de80a59360bc3ce3c398cb070e778d8d224
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-04-02 17:31:10 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+num = [[0 for _ in range(10)] for _ in range(N)]
+
+for i in range(1, 10) :
+    num[0][i] = 1
+
+for i in range(1, N) :
+    for j in range(10) :
+        if j != 9 : num[i][j] += num[i - 1][j + 1]
+        if j != 0 : num[i][j] += num[i - 1][j - 1]
+            
+print(sum(num[N-1]) % 1000000000)
