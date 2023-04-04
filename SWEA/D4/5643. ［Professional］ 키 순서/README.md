f8f5fa1311632bce1ceadcab67ab34af61605cf5
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-04-04 17:29:12 +0900

    [D4] Title: [Professional] 키 순서, Time: 963 ms, Memory: 94,252 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/README.md
@@ -0,0 +1,11 @@
+# [D4] [Professional] 키 순서 - 5643 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXQsLWKd5cDFAUo) 
+
+### 성능 요약
+
+메모리: 94,252 KB, 시간: 963 ms, 코드길이: 2,018 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static final int INF = 99999999;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		//첫 줄에 테스트케이스의 개수 T가 주어진다. (1 ≤ T ≤ 15)
+		st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		
+		for (int TC = 1; TC <= T; TC++) {
+			//각 테스트 케이스의 첫 번째 줄에 학생들의 수 N이 주어진다. (2 ≤ N ≤ 500)
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			
+			//각 테스트 케이스의 두 번째 줄에 두 학생의 키를 비교한 횟수 M이 주어진다. (0 ≤ M ≤ N*(N-1)/2)
+			st = new StringTokenizer(br.readLine());
+			int M = Integer.parseInt(st.nextToken());
+
+			int[][] map = new int[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				Arrays.fill(map[i], INF);
+			}
+			
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				//각 테스트 케이스의 세 번째 줄부터 M개의 줄에 걸쳐 두 학생의 키를 비교한 결과를 나타내는 두 양의 정수 a, b가 주어진다.
+				int a = Integer.parseInt(st.nextToken())-1;
+				int b = Integer.parseInt(st.nextToken())-1;
+				//이는 번호가 a인 학생이 번호가 b인 학생보다 키가 작은 것을 의미한다. 이 때, 입력은 항상 모순이 없도록 주어진다.
+				map[a][b] = 1;
+			}
+			
+			for (int k = 0; k < N; k++) {
+				for (int i = 0; i < N; i++) {
+					for (int j = 0; j < N; j++) {
+						map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+					}
+				}
+			}
+			
+			int result = 0;
+			for (int i = 0; i < N; i++) {
+				int cnt = 1;
+				for (int j = 0; j < N; j++) {
+					if(i==j) continue;
+					if((map[i][j] & map[j][i]) != INF) {
+						cnt++;
+						continue;
+					}
+					break;
+				}
+				if(cnt == N) result++;
+			}
+			
+			//자신이 키가 몇 번째인지 알 수 있는 학생이 모두 몇 명인지를 출력한다.
+			sb.append("#" + TC + " " + result + "\n");
+			
+		}
+		System.out.println(sb);
+		br.close();
+	}
+}
