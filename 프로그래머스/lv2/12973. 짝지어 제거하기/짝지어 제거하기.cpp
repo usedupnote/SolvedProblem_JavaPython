57b7c3959c9d7f0113b5b5ade33f86a66413ae7b
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-04-13 14:13:33 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.75 ms, Memory: 6.67 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = -1;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
