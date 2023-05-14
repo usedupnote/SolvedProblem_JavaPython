7bdeed8b81db04832d40cded0dfe88c7dbe705d2
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-05-14 14:28:31 +0900

    [Silver II] Title: 아기 상어 2, Time: 96 ms, Memory: 12068 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -2,52 +2,55 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] dirX = {0,-1,-1,-1,0,1,1,1}; // 좌 좌상 상 상우 우 우하 하 하좌
-	static int[] dirY = {-1,-1,0,1,1,1,0,-1};
+	static final int[] dirX = {-1,-1,0,1,1,1,0,-1};	//상 상우 우 우하 하 하좌 좌 좌상
+	static final int[] dirY = {0,1,1,1,0,-1,-1,-1};
+	static final int MV = Integer.MAX_VALUE;
+	
 	static int N, M;
 	static int[][] map;
-	static ArrayDeque<int[]> q;
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		q = new ArrayDeque<>();
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+	static ArrayDeque<int[]> q = new ArrayDeque<>();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        map = new int[N][M];
+        
+        for (int i = 0; i < N; i++) {
+        	st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				if(Integer.parseInt(st.nextToken()) == 1) {
-					map[i][j] = 1;
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j]==1) {
+					map[i][j] = 0;
 					q.offer(new int[] {i, j});
+				} else {
+					map[i][j] = MV;
 				}
 			}
 		}
-		
-		System.out.println(bfs());
-	}
-
-	private static int bfs() {
-		int[] poll = null;
-		while (!q.isEmpty()) {
-			poll = q.poll();
-			
+        int dx, dy;
+        int result = 0;
+        
+        while (!q.isEmpty()) {
+			int[] poll = q.poll();
 			for (int d = 0; d < 8; d++) {
-				int dx = poll[0] + dirX[d];
-				int dy = poll[1] + dirY[d];
+				dx = poll[0] + dirX[d];
+				dy = poll[1] + dirY[d];
 				
-				if(0<=dx && dx<N && 0<=dy && dy<M && map[dx][dy] == 0) {
-					map[dx][dy] = map[poll[0]][poll[1]] + 1;
+				if (0<=dx && dx<N && 0<=dy && dy<M && map[dx][dy] > map[poll[0]][poll[1]]+1) {
+					map[dx][dy] = map[poll[0]][poll[1]]+1;
 					q.offer(new int[] {dx,dy});
+					result = Math.max(result, map[dx][dy]);
 				}
 			}
 		}
-		return map[poll[0]][poll[1]] - 1;
-	}
+        System.out.println(result);
+        br.close();
+    }
 }
