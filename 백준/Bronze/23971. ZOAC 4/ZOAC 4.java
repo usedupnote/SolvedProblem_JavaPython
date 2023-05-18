aa4cee45d79644144f6d28bb3d53eb214e0815e1
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-05-18 23:56:39 +0900

    [Bronze III] Title: ZOAC 4, Time: 80 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		H+=N++; W+=M++;
+		System.out.println((H/N)*(W/M));
+		br.close();
+	}
+}
