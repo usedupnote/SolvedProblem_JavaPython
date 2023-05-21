07c988d2ded591b876e4877a4c2adda19f110cf1
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-05-21 14:54:02 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 546 ms, Memory: 28,704 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -15,14 +15,12 @@ public class Solution {
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 			
-			int[][] map = new int[N][N];
 			ArrayList<int[]> q = new ArrayList<>();
 			
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-					if (map[i][j] == 1) {
+					if (Integer.parseInt(st.nextToken()) == 1) {
 						q.add(new int[] {i, j});
 					}
 				}
@@ -30,14 +28,13 @@ public class Solution {
 			int qSize = q.size();
 			int maxPrice = qSize * M;
 			int result = 0;
-			
+
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					for (int d = 1; d <= N*2; d++) {
 						int compare = 0;
 						// 모든 경우에서 최대 얻을 수 있는 수익보다 운영비용이 클 경우 pass
 						if ((d*d)+(((d-1)*(d-1))) > maxPrice) break;
-//						System.out.println((d*d)+((d-1)*(d-1)) + " " + maxPrice);
 						for (int k = 0; k < qSize; k++) {
 							if (d <= Math.abs(q.get(k)[0] - i) + Math.abs(q.get(k)[1] - j)) {
 								continue;
@@ -45,7 +42,6 @@ public class Solution {
 							compare++;
 						}
 						if ((d*d)+((d-1)*(d-1)) > compare * M) continue;
-						map[i][j] = Math.max(map[i][j], compare);
 						result = Math.max(result, compare);
 					}
 				}
