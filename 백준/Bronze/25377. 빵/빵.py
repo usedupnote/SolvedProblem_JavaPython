af79224b5fa0d2e37d33a57741acc9dd5a4e3c20
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-05-31 23:24:58 +0900

    [Bronze IV] Title: 빵, Time: 136 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,17 +1,15 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-T = int(input())
+T=int(input())
 
-result = 100001
-
+result=100001
 for i in range(T) :
-    A, B = map(int, input().split())
-
-    if A - B <= 0 :
+    A,B=map(int, input().split())
+    if A-B<=0:
         result = min(B, result)
 
-if result == 100001 :
+if result==100001 :
     print(-1)
 else :
     print(result)
