4e55fd6dbaefa9c208d9040d98a8e008132700eb
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-06-13 22:56:38 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,35 +1,26 @@
 import java.io.*;
 import java.util.*;
- 
-public class Main {
+
+public class Main {	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;			
 		
-		int N = Integer.parseInt(st.nextToken());
-		
-		int[] day = new int[N + 1];
-		
-		st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-		int P = Integer.parseInt(st.nextToken());
-		
-		if (T <= N) day[T] = P;
-		
-		for (int i = 1; i < N; i++) {
+		int N = Integer.parseInt(br.readLine());
+				
+		int[] num = new int[N+6];
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			T = Integer.parseInt(st.nextToken());
-			P = Integer.parseInt(st.nextToken());
-
-			day[i] = Math.max(day[i-1], day[i]);
+			int d = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
 			
-			if (i + T <= N) {
-				day[i+T] = Math.max(day[i] + P, day[i+T]);
+			num[i+d] = Math.max(num[i+d], num[i] + s);
+			for (int j = i+d; j <= N; j++) {
+				num[j] = Math.max(num[j-1], num[j]);
 			}
 		}
-		day[N] = Math.max(day[N-1], day[N]);
-		
-		System.out.println(day[N]);
+		System.out.println(num[N]);
 		br.close();
 	}
 }
