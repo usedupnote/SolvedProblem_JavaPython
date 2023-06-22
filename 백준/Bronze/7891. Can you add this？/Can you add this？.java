c5650ab64fe65ae094fcece03bda6b233d18ac75
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-06-22 13:05:54 +0900

    [Bronze V] Title: Can you add this?, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(a + b).append("\n");
+		}
+		System.out.println(sb);
+        br.close();
+	}
+}
