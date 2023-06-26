87459b5c350789960d020f5ed4630cbbb0274662
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-06-27 01:10:52 +0900

    [Bronze IV] Title: 샤틀버스, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;			
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		System.out.println(N>M?M+N:M-N);
+		br.close();
+	}
+}
