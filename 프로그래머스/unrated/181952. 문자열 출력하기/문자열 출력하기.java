e662dd2b689f2a829d7c1f0a7a138f455412cb2c
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-07-22 23:42:09 +0900

    [unrated] Title: 문자열 출력하기, Time: 151.00 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
