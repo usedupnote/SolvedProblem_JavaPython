34f5020f7a39d058ddcec5c45c8700f04c02b539
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-07-23 00:00:35 +0900

    [unrated] Title: 문자열 돌리기, Time: 161.02 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char[] C = a.toCharArray();
+        
+        for(char c : C) System.out.println(c);
+    }
+}
