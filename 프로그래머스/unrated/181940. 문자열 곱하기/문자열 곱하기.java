b840d5ad3f7dfceba34a85f2e56f9fccdf071e04
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-07-23 00:04:17 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.05 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < k ; i++){
+            sb.append(my_string);
+        }
+        
+        return answer = sb.toString();
+    }
+}
