bf143f9729bc707801d0047b24c53e53c9474d70
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2023-12-24 23:38:43 +0900

    [Bronze I] Title: 등차수열의 합, Time: 224 ms, Memory: 42560 KB -BaekjoonHub

---
+++ 백준/Bronze/28115. 등차수열의 합/등차수열의 합.py
@@ -0,0 +1,22 @@
+T = int(input())
+num = list(map(int, input().split()))
+
+cur = -1
+state = "YES"
+if T > 2 :
+    cur = num[1]-num[0]
+    for i in range(1, T) :
+        if cur != num[i]-num[i-1] :
+            state = "NO"
+            break
+elif (T == 2) :
+    cur = num[1]-num[0]
+
+print(state)
+
+if state == "YES" :
+    for i in range(T) :
+        print(num[i] * 2, end=" ")
+    print()
+    for i in range(T) :
+        print(num[i] * -1, end=" ")
