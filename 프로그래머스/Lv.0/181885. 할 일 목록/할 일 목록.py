5318037dffcc0960e8fb5e095d0e141e0af4c905
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2024-01-15 20:29:16 +0900

    [level Lv.0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,7 @@
+def solution(todo_list, finished):
+    answer = []
+    
+    for i in range(len(finished)) :
+        if not finished[i] :
+            answer.append(todo_list[i])
+    return answer
