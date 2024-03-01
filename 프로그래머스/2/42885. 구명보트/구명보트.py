d7de46c7d6ae49ef94adfd29fdb8c2ee38bf9bc0
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2024-03-01 18:33:01 +0900

    [level 2] Title: 구명보트, Time: 8.80 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    
+    start = 0
+    end = len(people) - 1
+    
+    while start < end :
+        answer += 1
+        if people[start] + people[end] <= limit:
+            start += 1
+        end -= 1
+        
+    if start == end:
+        answer += 1
+        
+    return answer
