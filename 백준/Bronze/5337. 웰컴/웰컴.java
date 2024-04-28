707cd0e3167250caa4135539db6fd47a0042d2b1
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2024-04-28 12:58:43 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11188 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n"+"|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
