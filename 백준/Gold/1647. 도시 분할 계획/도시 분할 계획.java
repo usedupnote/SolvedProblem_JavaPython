94a7e6a97a3288d4962cc653c8811330f274f0e6
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2025-03-30 15:53:37 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2144 ms, Memory: 353140 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -34,7 +34,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
 		
 		int result = 0;
 		int maxValue = 0;
