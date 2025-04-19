ebe3bc494fa5daf1f90b2cc77d7cec2308282c09
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2025-04-19 12:56:33 +0900

    [Silver I] Title: 신입 사원, Time: 2080 ms, Memory: 312204 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -3,37 +3,41 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    StringBuilder sb = new StringBuilder();
-	    StringTokenizer st = new StringTokenizer(br.readLine());
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-	    int T = Integer.parseInt(st.nextToken());
-	    
-	    for (int test_case = 0; test_case < T; test_case++) {
-	    	st = new StringTokenizer(br.readLine());
-	    	int N = Integer.parseInt(st.nextToken());
-	    	
-	    	int[] num = new int[N];
-	    	
-	    	for (int i = 0; i < N; i++) {
-	    		st = new StringTokenizer(br.readLine());
-	    		int A = Integer.parseInt(st.nextToken()) - 1;
-	    		int B = Integer.parseInt(st.nextToken()) - 1;
-		    	
-	    		num[A] = B;
+        int T = Integer.parseInt(st.nextToken());
+        for (int t = 0; t < T; t++) {
+        	st = new StringTokenizer(br.readLine());
+        	int N = Integer.parseInt(st.nextToken());
+        	int cnt = 0;
+        	
+        	PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
+        		@Override
+        		public int compare(int[] o1, int[] o2) {
+        			return Integer.compare(o1[0], o2[0]);
+        		}
+			});
+        	
+        	for (int i = 0; i < N; i++) {
+        		st = new StringTokenizer(br.readLine());
+				q.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
 			}
-	    	
-	    	int check = num[0], result = 1;
-			
-	    	for (int i = 1; i < N; i++) {
-				if(num[i] < check) {
-					check = num[i];
-					result++;
+        	
+        	int cur = Integer.MAX_VALUE;
+        	while (!q.isEmpty()) {
+				int[] poll = q.poll();
+				
+				if (cur > poll[1]) {
+					cur = poll[1];
+					cnt++;
 				}
 			}
-	    	sb.append(result + "\n");
+        	sb.append(cnt).append("\n");
 		}
-	    System.out.println(sb);
-	    br.close();
+        
+        System.out.println(sb);
+        br.close();
 	}
 }
