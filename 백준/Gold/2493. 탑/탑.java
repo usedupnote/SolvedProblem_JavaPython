298352bd00007a586bba88b9874260dc71f30fbe
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2025-04-28 07:54:11 +0900

    [Gold V] Title: 탑, Time: 668 ms, Memory: 114260 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,39 +1,34 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Main {
-	static int N;
-	static int[] num;
-	static ArrayDeque<int[]> maxPoint = new ArrayDeque<>();
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		
-		num = new int[N];
-		
-		maxPoint.offer(new int [] {100000000, 0});
-		st = new StringTokenizer(br.readLine());
-		
-		for (int i = 0; i < N; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(st.nextToken());
+        
+        Stack<int[]> s = new Stack<>();
+        
+        s.add(new int[] {100_000_001, 0});
+        
+        st = new StringTokenizer(br.readLine());
+        
+        for (int i = 1; i <= N; i++) {
+			int height = Integer.parseInt(st.nextToken());
 			
-			while (!maxPoint.isEmpty()) {
-				if (num[i] > maxPoint.getFirst()[0]) {
-					maxPoint.poll();
-				} else {
-					sb.append((maxPoint.getFirst()[1]) + " ");
-					maxPoint.push(new int [] {num[i],i + 1});
-					break;
-				}
+			int[] pop = s.pop();
+        	while (pop[0] < height) {
+        		pop = s.pop();
 			}
+        	s.add(pop);
+			sb.append(s.peek()[1]).append(" ");
+			s.add(new int[] {height, i});
 		}
-		
-		System.out.println(sb);
-		
-		br.close();
+        
+        System.out.println(sb);
+        br.close();
 	}
 }
